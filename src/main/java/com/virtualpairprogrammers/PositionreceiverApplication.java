7a54cdfd133c9c2b62fc128acb7b1b11395c5dbf
VPPCourseUser <contact@capabilitytm.com>
2017-02-13 19:39:24 +0000

    Update PositionreceiverApplication.java

--- src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
+++ src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
@@ -20,13 +20,15 @@ public class PositionreceiverApplication {
 		SpringApplication.run(PositionreceiverApplication.class, args);
 	}
 
-	@Bean
+        @Bean
         public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
         {
+           EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils)
            AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
            instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
            instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
            instance.setDataCenterInfo(info);
            instance.setNonSecurePort(port);
-	}
+           return instance;
+        }
 }
