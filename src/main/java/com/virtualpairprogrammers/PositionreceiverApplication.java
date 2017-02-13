c1df29434da5e2ed7d059fad36e7639115f71e81
VPPCourseUser <contact@capabilitytm.com>
2017-02-13 19:51:12 +0000

    Update PositionreceiverApplication.java

--- src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
+++ src/main/java/com/virtualpairprogrammers/PositionreceiverApplication.java
@@ -13,22 +13,8 @@ import com.netflix.appinfo.AmazonInfo;
 @EnableDiscoveryClient
 public class PositionreceiverApplication {
 
-	@Value("${server.port}")
-        private int port;
-	
 	public static void main(String[] args) {
 		SpringApplication.run(PositionreceiverApplication.class, args);
 	}
 
-        @Bean
-        public EurekaInstanceConfigBean eurekaInstanceConfigBean(InetUtils utils) 
-        {
-           EurekaInstanceConfigBean instance = new EurekaInstanceConfigBean(utils);
-           AmazonInfo info = AmazonInfo.Builder.newBuilder().autoBuild("eureka");
-           instance.setHostname(info.get(AmazonInfo.MetaDataKey.publicHostname));
-           instance.setIpAddress(info.get(AmazonInfo.MetaDataKey.publicIpv4));
-           instance.setDataCenterInfo(info);
-           instance.setNonSecurePort(port);
-           return instance;
-        }
 }
