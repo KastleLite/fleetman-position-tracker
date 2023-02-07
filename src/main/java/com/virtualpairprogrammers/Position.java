8542612d32ae9bf43b06040fd928b17105b576cc
KastleLite <ryan.mesics@gmail.com>
2023-02-07 15:32:32 -0500

    Changed the reference from javax to jakarta for XmlRootElement

--- src/main/java/com/virtualpairprogrammers/Position.java
+++ src/main/java/com/virtualpairprogrammers/Position.java
@@ -6,7 +6,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import javax.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlRootElement;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
